9b62acbdd378e9fff934080d980381ebfed02f78
nyoungstudios <nathanielyoung95070@gmail.com>
2018-07-01 15:58:52 -0700

    updates api call

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 function api_call(input) {
     $.ajax({
 //        url: "http://0.0.0.0:5000/importantWords?url=http://www.fullychargedshow.co.uk/electric-cars/coal-is-king",
-        url: "http://0.0.0.0:5000/importantWords?url=" + input,
-//        url: "https://tf-idf.herokuapp.com/importantWords?url=" + input + "&top=15",
+//        url: "http://0.0.0.0:5000/importantWords?url=" + input,
+        url: "https://tf-idf.herokuapp.com/importantWords?url=" + input + "&top=15",
         method: 'GET'
     }).done(function(data) {
         console.log(data);
